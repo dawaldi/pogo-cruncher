2d781030be91fda015d4bb6454ab23ff3933d7cc
Alexander Schneider <alexander@schneider.gg>
2016-08-03 23:44:11 +0200

    fuck christian, lel

--- frontend/src/cruncher/utils/lib.cljs
+++ frontend/src/cruncher/utils/lib.cljs
@@ -223,5 +223,5 @@
   "Convert String to Integer."
   [s]
   (let [converted (js/parseInt s)]
-    (when-not (js/isNaN converted)
-      converted)))
+    (is-not (js/isNaN converted)
+      converted )))
